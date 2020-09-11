c3ccc0dc3792e154fd11224ff64813e81e963d8e
Victor Sampaio <vsampaio@pm.me>
2020-09-11 18:04:15 -0300

    Fixing some problems

--- src/controller/food.controller.ts
+++ src/controller/food.controller.ts
@@ -6,20 +6,16 @@ class Food {
   async get(req: Request, res: Response) {
     console.log('Getting food request');
     try {
-      const { food: name } = req.params;
+      if (!req.params.food) return res.status(401).send('Missing parameter food');
 
-      if (!name) throw new Error('Missing parameter food');
+      const { food: name } = req.params;
 
       let result = await Foodstore.get(name);
 
       if (Object.keys(result).length == 0) {
-        const { data: foods } = await axios.get(
-          `https://platform.fatsecret.com/rest/server.api?method=foods.search&search_expression=${name}&weight=100&format=json`
-        );
+        const allFoodFound = await this.getFoodsAPI(name);
 
-        const bestOptions = foods.foods.food.filter((food: { food_type: string }) => {
-          return food.food_type === 'Generic';
-        });
+        const bestOptions = this.filterBetterOptions(allFoodFound);
 
         const foodID = bestOptions[0].food_id;
 
@@ -27,22 +23,7 @@ class Food {
           `https://platform.fatsecret.com/rest/server.api?method=food.get.v2&food_id=${foodID}&format=json`
         );
 
-        const serving100grams = apiFood.food.servings.serving.filter(
-          (serving: { serving_description: string }) => {
-            return serving.serving_description == '100 g';
-          }
-        );
-
-        return res.send({
-          name: apiFood.food.food_name,
-          id: foodID,
-          carbs: serving100grams[0].carbohydrate,
-          protein: serving100grams[0].protein,
-          fat: serving100grams[0].fat,
-          fiber: serving100grams[0].fiber,
-          sugar: serving100grams[0].sugar,
-          isCached: false,
-        });
+        return res.send({ ...this.getServing100grams(apiFood), id: foodID });
       }
 
       return res.send(result);
@@ -62,6 +43,40 @@ class Food {
     } catch (error) {
       return res.status(500).send('Internal Server ' + error);
     }
+  }
+
+  private async getFoodsAPI(name: string) {
+    const { data: foods } = await axios.get(
+      `https://platform.fatsecret.com/rest/server.api?method=foods.search&search_expression=${name}&weight=100&format=json`
+    );
+
+    if (foods.foods.total_results === '0') throw new Error('Nothing Found!');
+
+    return foods.foods.food;
+  }
+
+  private filterBetterOptions(foods: any[]) {
+    return foods.filter((food: { food_type: string }) => {
+      return food.food_type === 'Generic';
+    });
+  }
+
+  private getServing100grams(food: { food: { servings: { serving: any[] }; food_name: string } }) {
+    const serving100grams = food.food.servings.serving.filter(
+      (serving: { serving_description: string }) => {
+        return serving.serving_description == '100 g';
+      }
+    );
+
+    return {
+      name: food.food.food_name,
+      carbs: serving100grams[0].carbohydrate,
+      protein: serving100grams[0].protein,
+      fat: serving100grams[0].fat,
+      fiber: serving100grams[0].fiber,
+      sugar: serving100grams[0].sugar,
+      isCached: false,
+    };
   }
 }
 export default Food;
--- src/routes/food.route.ts
+++ src/routes/food.route.ts
@@ -4,7 +4,7 @@ import Food from '../controller/food.controller';
 const foodC = new Food();
 const food = express.Router();
 
-food.post('/:food', foodC.get);
-food.post('/new/save', foodC.save);
+food.post('/:food', foodC.get.bind(foodC));
+food.post('/new/save', foodC.save.bind(foodC));
 
 export default food;
